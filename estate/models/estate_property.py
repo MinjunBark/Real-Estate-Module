f85d8af414de327fb910dabbfaf21e56b20e7c79
MinjunBark <alexminjunlee@gmail.com>
2025-10-04 20:46:33 -0700

    Update estate_property.py
    added more fields for estate table

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -4,4 +4,21 @@ class EstateProperty(models.Model):
    _name = 'estate.property'
    _description = 'Estate Property'
    
-   name = fields.Char(string = "Name", required = True)
+   
+   name = fields.Char('Name', required=True, translate=True)
+   description  = fields.Text('Description', required=True)
+   postcode = fields.Char('Postcode', required=True)
+   date_availability = fields.Date('Date Availability', required=True)
+   expected_price = fields.Float('Expected Price', required=True)
+   selling_price = fields.Float('Selling Price', required=True)
+   bedrooms = fields.Integer('Bedrooms', required=True)
+   living_area = fields.Integer('Living Area', required=True)
+   facades = fields.Integer('Facades', required=True)
+   garage = fields.Boolean('Garage', required=True)
+   garden = fields.Boolean('Garden', required=True)
+   garden_area = fields.Integer('Garden Area', required=True)
+   garden_orientation = fields.Selection(
+       string = 'Garden Orientation',
+       selection = [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+       help = "The direction of the garden"
+   )
